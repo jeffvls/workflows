69390144b6efb91c7f430fd5b74cb982a4cd7541
Jeff van Loben Sels <jeff@umlaut.co>
2015-02-21 17:49:02 -0800

    Added static monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require ('gulp'),
     coffee = require('gulp-coffee'),
     compass = require('gulp-compass'),
     concat = require('gulp-concat'),
+    connect = require('gulp-connect'),
     browserify = require('gulp-browserify');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -13,6 +14,8 @@ var jsSources = [
 	'components/scripts/template.js',
 ];
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 
 gulp.task('coffee', function() {
@@ -24,26 +27,50 @@ gulp.task('coffee', function() {
 
 gulp.task('js', function() {
 	gulp.src(jsSources)
-	.pipe(concat('script.js'))
-	.pipe(browserify())
-	.pipe(gulp.dest('builds/development/js'))
+		.pipe(concat('script.js'))
+		.pipe(browserify())
+		.pipe(gulp.dest('builds/development/js'))
+		.pipe(connect.reload())
 });
 
 gulp.task('compass', function() {
 	gulp.src(sassSources)
-	.pipe(compass({
-		sass: 'components/sass',
-		image: 'builds/development/images',
-		style: 'expanded'
-	})
-		.on('error', gutil.log))
-	.pipe(gulp.dest('builds/development/css'))
+		.pipe(compass({
+			sass: 'components/sass',
+			image: 'builds/development/images',
+			style: 'expanded'
+		})
+			.on('error', gutil.log))
+		.pipe(gulp.dest('builds/development/css'))
+		.pipe(connect.reload())
 });
 
+gulp.task('connect', function() {
+	connect.server({
+		root: 'builds/development/',
+		livereload: true
+	})
+});
+
+gulp.task('html', function() {
+	gulp.src(htmlSources)
+	.pipe(connect.reload())
+})
+
+gulp.task('json', function() {
+	gulp.src(jsonSources)
+	.pipe(connect.reload())
+})
+
+
 gulp.task('watch', function() {
+	gulp.watch( htmlSources, ['html'] );
 	gulp.watch( coffeeSources, ['coffee'] );
 	gulp.watch( jsSources, ['js'] );
+	gulp.watch( htmlSources, ['html'] );
+	gulp.watch( jsonSources, ['json'] );
 	gulp.watch( 'components/sass/*.scss', ['compass'] );
 });
 
-gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
+
+gulp.task('default', ['html', 'coffee', 'js', 'json', 'compass', 'connect', 'watch']);
